af2d40beec3b0e79c4df9773acdbc60ed2e62070
Jay Rajshakha <jayrajshakha01@gmail.com>
2023-10-14 17:18:24 +0530

    Cast Done

--- src/pages/detailsPage/cast/Cast.jsx
+++ src/pages/detailsPage/cast/Cast.jsx
@@ -25,7 +25,36 @@ const Cast = ({ data, loading }) => {
   return (
     <div className="castSection">
       <ContentWrapper>
-         
+      <div className="sectionHeading">Top Cast</div>
+      {!loading ? (
+                    <div className="listItems">
+                        {data?.map((item) => {
+                            let imgUrl = item.profile_path
+                                ? url.profile + item.profile_path
+                                : avatar;
+                            return (
+                                <div key={item.id} className="listItem">
+                                    <div className="profileImg">
+                                        <Img src={imgUrl} />
+                                    </div>
+                                    <div className="name">{item.name}</div>
+                                    <div className="character">
+                                        {item.character}
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <div className="castSkeleton">
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                    </div>
+                )}
       </ContentWrapper>
     </div>
   );
